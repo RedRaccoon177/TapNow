84a90404c792474f2a3bb49e7a0b3b37b3d63cd7
Fifow <lasldjf123@naver.com>
2025-04-30 16:51:36 +0900

    tt

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -3,8 +3,8 @@ using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour
 {
-    public void LoadScene(string sceneName)
+    public void LoadScene()
     {
-        SceneManager.LoadScene(sceneName);
+        SceneManager.LoadScene("InGameScene");
     }
 }
